7bf22db7549f752f9e14426f706b78c8578c650b
Joel Butler <joel.e.butler04@gmail.com>
2025-03-22 22:03:52 -0400

    Update TempoKnob.jsx
    - Fixed default value overridden by debounce
    - Percentage matches now too

--- src/renderer/src/components/TempoKnob.jsx
+++ src/renderer/src/components/TempoKnob.jsx
@@ -1,9 +1,15 @@
+/* eslint-disable react/display-name */
 import { Knob, Pointer, Scale } from 'rc-knob'
 import React, { useState, useCallback } from 'react'
 import { debounce } from 'lodash'
 
 const TempoKnob = React.memo(({ onChange }) => {
-  const [value, setValue] = useState(60)
+  const [value, setValue] = useState(120) // Default tempo value
+  const [firstPass, toggleFirstPass] = useState(true)
+  if (firstPass) {
+    onChange(value)
+    toggleFirstPass(false)
+  }
 
   const debouncedOnChange = useCallback(
     debounce((newValue) => {
@@ -29,13 +35,20 @@ const TempoKnob = React.memo(({ onChange }) => {
         angleRange={280}
         steps={10}
         min={60}
-        max={260}
-        value={value}
+        max={180}
+        initialValue={120}
         onChange={handleChange}
       >
         <Scale tickWidth={2} tickHeight={2} radius={45} />
         <circle r="35" cx="50" cy="50" fill="#FC5A96" />
-        <Pointer width={2} height={35} radius={10} type="rect" color="#FC5A96" />
+        <Pointer
+          width={2}
+          height={35}
+          radius={10}
+          type="rect"
+          color="#FC5A96"
+          percentage={(value - 60) / 120} // Ensure the pointer matches the knob value
+        />
       </Knob>
       <label>Tempo: {value}</label>
     </div>
